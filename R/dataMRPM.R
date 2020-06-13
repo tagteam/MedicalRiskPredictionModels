94d57f8f82e35b48ac70ec3c9932da24688dcb7b
Thomas Alexander Gerds <tag@biostat.ku.dk>
2020-06-13 12:13:51 +0200

    data loading

--- R/dataMRPM.R
+++ R/dataMRPM.R
@@ -1,7 +1,7 @@
 ## Version: 
-## Last-Updated: Jun 13 2020 (11:35) 
+## Last-Updated: Jun 13 2020 (12:10) 
 ##           By: Thomas Alexander Gerds
-##     Update #: 93
+##     Update #: 117
 #----------------------------------------------------------------------
 ## 
 ### Commentary: 
@@ -10,75 +10,85 @@
 #----------------------------------------------------------------------
 ## 
 ### Code:
+##' Loads 4 data sets to illustrate the R-code of the book Medical Risk Prediction Models by Gerds & Kattan
+##'
+##' The data are synthetic data generated with the computer. 
+##' @title load data for Medical Risk Prediction Models
+##' @return 4 data sets  
+##' @examples
+##' dataMRPM()
+##' @export 
+##' @author Thomas A. Gerds <tag@@biostat.ku.dk>
 dataMRPM <- function(){
     options(width=70)
-    suppressMessages(library(prodlim))
-    suppressMessages(library(survival,verbose=0L))
-    suppressMessages(library(SuperLearner,verbose=0L))
-    suppressMessages(library(data.table,verbose=0L))
-    suppressMessages(library(Publish))
-    suppressMessages(library(foreach))
-    suppressMessages(library(gridExtra))
-    suppressMessages(library(bootstrap))
-    suppressMessages(library(plotrix))
-    suppressMessages(library(randomForestSRC))
-    suppressMessages(library(randomForest))
-    suppressMessages(library(ranger))
-    suppressMessages(library(neuralnet))
-    suppressMessages(library(nnet))
-    suppressMessages(library(cowplot))
-    suppressMessages(library(gridGraphics))
-    suppressMessages(library(party))
-    suppressMessages(library(penalized,verbose=0L))
-    suppressMessages(library(rms,verbose=0L))
-    suppressMessages(library(riskRegression,verbose=0L))
-    suppressMessages(library(ggplot2,verbose=0L))
-    suppressMessages(library(ggpubr,verbose=0L))
-    suppressMessages(library(rpart,verbose=0L))
-    suppressMessages(library(lattice,verbose=0L))
-    suppressMessages(library(grid,verbose=0L))
+    library(prodlim)
+    library(survival)
+    library(SuperLearner)
+    library(data.table)
+    library(Publish)
+    library(foreach)
+    library(gridExtra)
+    library(bootstrap)
+    library(plotrix)
+    library(randomForestSRC)
+    library(randomForest)
+    library(ranger)
+    library(neuralnet)
+    library(nnet)
+    library(cowplot)
+    library(gridGraphics)
+    library(party)
+    library(penalized)
+    library(rms)
+    library(riskRegression)
+    library(ggplot2)
+    library(ggpubr)
+    library(rpart)
+    library(lattice)
+    library(grid)
     theme_set(theme_pubr())
     # --------------------------------------------------------------------------------------------------
     ## in vitro fertilization study
     # --------------------------------------------------------------------------------------------------
+    requireNamespace("data.table")
     data(ivf)
-    setDT(ivf)
-    ivf[,smoking:=factor(smoking,levels=c("No","Yes"),labels=c("No","Yes"))]
-    ivf[,OHSS:=factor(OHSS,levels=c("No","Yes"),labels=c("No","Yes"))]
+    data.table::setDT(ivf)
+    ivf$smoking  <- factor(ivf$smoking,levels=c("No","Yes"),labels=c("No","Yes"))
+    ivf$OHSS <- factor(ivf$OHSS,levels=c("No","Yes"),labels=c("No","Yes"))
     set.seed(17)
-    ivf[,train:=1:NROW(ivf)%in%sample(1:NROW(ivf),replace=FALSE,size=round(0.63*NROW(ivf)))]
-    ivf[,train:=factor(train,levels=c("FALSE","TRUE"),labels=c("FALSE","TRUE"))]
-    ivftrain <- ivf[train==TRUE]
-    ivftest <- ivf[train==FALSE]
+    ivf$train  <- 1:NROW(ivf)%in%sample(1:NROW(ivf),replace=FALSE,size=round(0.63*NROW(ivf)))
+    ivftrain  <- factor(ivf$train,levels=c("FALSE","TRUE"),labels=c("FALSE","TRUE"))
+    ivftrain <- ivf[ivf$train==TRUE,]
+    ivftest <- ivf[ivf$train==FALSE,]
     # --------------------------------------------------------------------------------------------------
     # oral cancer study
     # --------------------------------------------------------------------------------------------------
     data(oc)
     setDT(oc)
-    oc[,grade:=factor(grade,levels=c("Well","Moderate","Poor"))]
-    oc[,gender:=factor(gender,levels=c("Female","Male"),labels=c("Female","Male"))]
-    oc[,tobacco:=factor(tobacco,levels=c("","Ever","Never"),labels=c("","Ever","Never"))]
-    oc[,deep.invasion:=factor(deep.invasion,levels=c("","No","Yes"),labels=c("","No","Yes"))]
-    oc[,site:=factor(site,levels=c("Buccal Mucosa","Floor of Mouth","Hard Palate","Lower Gum","Retromolar Trigone","Tongue","Upper Gum"),labels=c("Buccal Mucosa","Floor of Mouth","Hard Palate","Lower Gum","Retromolar Trigone","Tongue","Upper Gum"))]
-    oc[,race:=factor(race,levels=c("Caucasian","NonCauc"),labels=c("Caucasian","NonCauc"))]
-    oc[,vascular.invasion:=factor(vascular.invasion,levels=c("","No","Yes"),labels=c("","No","Yes"))]
-    oc[,Grade:=factor(Grade,levels=c("Moderate/Well","Poor"),labels=c("Moderate/Well","Poor"))]
+    oc$grade=factor(oc$grade,levels=c("Well","Moderate","Poor"))
+    oc$gender=factor(oc$gender,levels=c("Female","Male"),labels=c("Female","Male"))
+    oc$tobacco=factor(oc$tobacco,levels=c("","Ever","Never"),labels=c("","Ever","Never"))
+    oc$deep.invasion=factor(oc$deep.invasion,levels=c("","No","Yes"),labels=c("","No","Yes"))
+    oc$site=factor(oc$site,levels=c("Buccal Mucosa","Floor of Mouth","Hard Palate","Lower Gum","Retromolar Trigone","Tongue","Upper Gum"),labels=c("Buccal Mucosa","Floor of Mouth","Hard Palate","Lower Gum","Retromolar Trigone","Tongue","Upper Gum"))
+    oc$race=factor(oc$race,levels=c("Caucasian","NonCauc"),labels=c("Caucasian","NonCauc"))
+    oc$vascular.invasion=factor(oc$vascular.invasion,levels=c("","No","Yes"),labels=c("","No","Yes"))
+    oc$Grade=factor(oc$Grade,levels=c("Moderate/Well","Poor"),labels=c("Moderate/Well","Poor"))
     # split into train and test
     set.seed(17)
-    oc[,train:=1:NROW(oc)%in%sample(1:NROW(oc),replace=FALSE,size=round(0.63*NROW(oc)))]
-    octrain <- oc[train==TRUE]
-    octest <- oc[train==FALSE]
+    oc$train=1:NROW(oc)%in%sample(1:NROW(oc),replace=FALSE,size=round(0.63*NROW(oc)))
+    octrain <- oc[oc$train==TRUE,]
+    octest <- oc[oc$train==FALSE,]
     # complete case analysis
     oc.cc <- na.omit(oc)
     octrain.cc <- na.omit(octrain)
     octest.cc <- na.omit(octest)
     # dummy variables
     ocform <- Surv(survtime,survstatus)~grade+age+tumorthickness+gender+tobacco+deep.invasion+site+race+x.posnodes+tumormaxdimension+vascular.invasion
-    octest.dummy <- cbind(octest.cc[,.(survtime,survstatus)],model.matrix(ocform,octest.cc)[,-1])
+    octest.dummy <- cbind(octest.cc[,c("survtime","survstatus")],model.matrix(ocform,octest.cc)[,-1])
     setnames(octest.dummy,gsub(" ",".",names(octest.dummy)))
-    octrain.dummy <- cbind(octrain.cc[,.(survtime,survstatus)],model.matrix(ocform,octrain.cc)[,-1])
+    octrain.dummy <- cbind(octrain.cc[,c("survtime","survstatus")],model.matrix(ocform,octrain.cc)[,-1])
     setnames(octrain.dummy,gsub(" ",".",names(octrain.dummy)))
-    oc.dummy <- cbind(oc.cc[,.(survtime,survstatus)],model.matrix(ocform,oc.cc)[,-1])
+    oc.dummy <- cbind(oc.cc[,c("survtime","survstatus")],model.matrix(ocform,oc.cc)[,-1])
     setnames(oc.dummy,gsub(" ",".",names(oc.dummy)))
     # --------------------------------------------------------------------------------------------------
     # active surveillance prostate cancer study
@@ -86,28 +96,29 @@ dataMRPM <- function(){
     data(as)
     setDT(as)
     set.seed(17)
-    as[,train:=1:NROW(as)%in%sample(1:NROW(as),replace=FALSE,size=round(0.63*NROW(as)))]
-    as[,asprog:=factor(asprog,levels=0:2,labels=c("0","progression","death"))]
+    as$train=1:NROW(as)%in%sample(1:NROW(as),replace=FALSE,size=round(0.63*NROW(as)))
+    as$asprog=factor(as$asprog,levels=0:2,labels=c("0","progression","death"))
     setnames(as,"lpsaden","psa")
-    astrain <- as[train==TRUE]
-    astest <- as[train==FALSE]
+    astrain <- as[as$train==TRUE,]
+    astest <- as[as$train==FALSE,]
     # --------------------------------------------------------------------------------------------------
     ## psa longitudinal form
     # --------------------------------------------------------------------------------------------------
     data(long)
     setDT(long)
-    long[,psadate:=as.Date(psadate,format="%Y-%m-%d")]
+    long$psadate <- as.Date(long$psadate,format="%Y-%m-%d")
     psadt <- function(psa.time,psa.val){(log(2)/coef(lm(log(psa.val)~psa.time))[2])/365.25}
     setkey(long,subject,psadate)
-    long[,psa.doublingstime:=psadt(psa.time=psadate,psa.val=psa),by=subject]
+    # long[,psa.doublingstime:=psadt(psa.time=psadate,psa.val=psa),by=subject]
     # --------------------------------------------------------------------------------------------------
     ## example time to event data
     # --------------------------------------------------------------------------------------------------
-    d <- data.table(id=c(1:5),
+    d <- data.frame(id=c(1:5),
                     af.date=c("2001-04-25","1995-02-16","2001-09-09","1999-12-20","1997-05-27"),
                     death.date=c(NA,"2011-10-27",NA,"2009-01-02",NA),
                     stroke.date=c("2005-11-16",NA,NA,"2007-09-01","1999-12-18"),
                     lost.date=c(NA,NA,NA,NA,"2008-08-17"))
+    setDT(d)
     # --------------------------------------------------------------------------------------------------
     # output message
     # --------------------------------------------------------------------------------------------------
